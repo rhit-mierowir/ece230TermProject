152d9e23ed65e4110d3185b6e87f825a3912689e
Isaac Mierow <mierowir@rose-hulman.edu>
2024-02-03 00:23:00 -0500

    I have verified that the hardware works correctly,
    and figured out whether the sensor triggers when the tank is empty or
    full.

--- ece230TermProjectMierowIZawaidehH/ece230TermProject.c
+++ ece230TermProjectMierowIZawaidehH/ece230TermProject.c
@@ -21,6 +21,8 @@
  *            |            P2.1  |---> LED2 green
  *            |            P2.2  |---> LED2 blue
  *            |                  |
+ *            |            P1.5  |---> Water Level Sensor
+ *            |            P2.7  |---> Motor Output ()
  *            |                  *****************************************************************/
 
 
@@ -39,17 +41,23 @@
 #define WATER_BUTTON_PORT P1
 #define WATER_BUTTON_MASK 0b10
 
-//Water Level Sensor Constants
+//Water Level Sensor Constants P1.5
 #define WATER_SENSOR_PORT P1
-#define WATER_SENSOR_MASK 0b10000
+#define WATER_SENSOR_MASK 0b100000
 
-boolean watering = 0; //is the watering system currently on
+//Water Motor Constants
+#define WATER_PUMP_PORT P2
+#define WATER_PUMP_MASK (0b1 << 7)
+
+_Bool watering = 0; //is the watering system currently on
 
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 	configureLEDs();
 	configureButtons();
+	configureLevelSensor();
+	configPump();
 
 	while(1){
 	    detectWateringButtonPress();
@@ -97,22 +105,35 @@ void configureButtons(void)
  * This also toggles the B_RGB_LED to on if the system is active, off otherwise.
  */
 
+void configPump(){
+    //Set output, DIR=1
+    WATER_PUMP_PORT->DIR |= WATER_PUMP_MASK;
+
+    //Set to default GPIO Pins SEL=0,0
+    WATER_PUMP_PORT->SEL0 &= ~WATER_PUMP_MASK;
+    WATER_PUMP_PORT->SEL1 &= ~WATER_PUMP_MASK;
+
+    //Start with lights off OUT=0
+    WATER_PUMP_PORT->OUT &= ~WATER_PUMP_MASK;
+}
+
 void detectWateringButtonPress(){
     if(WATER_BUTTON_PORT->IN & WATER_BUTTON_MASK) // if button is High, so not pressed
         return;
     //toggle the watering state;
-    if (watering)
-    {
+    if (watering){
         stopWatering();
     }else{
         startWatering();
     }
     debounce();
-    while(WATER_BUTTON_PORT->IN & WATER_BUTTON_MASK); // wait until button no longer pressed
+    while(!(WATER_BUTTON_PORT->IN & WATER_BUTTON_MASK)); // wait until button no longer pressed
     debounce();
 }
 
 // Set Debounce values for script.
+//Handling Delays internally and not with timers.
+#define SYSTEM_CLOCK_FREQUENCY 3000  //kHz  (3MHz)
 #define DEBOUNCE_PERIOD 5 //milliseconds
 #define DEBOUNCE_LOOP_CYCLES 8 //cycles
 #define DEBOUNCE_LOOP_COUNT DEBOUNCE_PERIOD*SYSTEM_CLOCK_FREQUENCY/DEBOUNCE_LOOP_CYCLES
@@ -131,17 +152,19 @@ void startWatering(void)
     watering = 1;
 
     // turn on watering system
+    WATER_PUMP_PORT->OUT |= WATER_PUMP_MASK;
 }
 
 void stopWatering(void)
 {
     //Set LED1 to off
-    RGB_LED_PORT->OUT &= B_LED_MASK;
+    RGB_LED_PORT->OUT &= ~B_LED_MASK;
 
     //Report Turned off watering
     watering=0;
 
     // turn off watering system
+    WATER_PUMP_PORT->OUT &= ~WATER_PUMP_MASK;
 }
 
 void configureLevelSensor()
@@ -163,6 +186,13 @@ void configureLevelSensor()
 void readWaterLevelSensor()
 {
     //Read the Sensor and condition on it to determine if the
+    // If pin is high, switch not pressed
+    if ( WATER_SENSOR_PORT->IN & WATER_SENSOR_MASK ) {
+        waterLevelGood();
+    } else {
+        waterLevelLow();
+    }
+
 }
 
 void waterLevelLow()
