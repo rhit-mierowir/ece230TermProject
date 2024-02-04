bbc3355b451e3f88a7fd037f66e0f7d8e7f8fca2
Isaac Mierow <mierowir@rose-hulman.edu>
2024-02-04 13:02:34 -0500

    header file.

---
+++ ece230TermProjectMierowIZawaidehH/ece230TermProject.h
@@ -0,0 +1,15 @@
+/*
+ * ece230TermProject.h
+ *
+ *  Created on: Feb 4, 2024
+ *      Author: mierowir
+ */
+
+#ifndef ECE230TERMPROJECT_H_
+#define ECE230TERMPROJECT_H_
+
+
+
+
+
+#endif /* ECE230TERMPROJECT_H_ */
