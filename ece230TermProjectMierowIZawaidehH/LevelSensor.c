ca971290c6e7a569e458aac97101cfb52fe9edd9
zawaidhm <zawaidhm@137.112.204.191>
2024-02-15 19:57:48 -0500

    created level sensor files

---
+++ ece230TermProjectMierowIZawaidehH/LevelSensor.c
@@ -0,0 +1,8 @@
+/*
+ * LevelSensor.c
+ *
+ *  Created on: Feb 15, 2024
+ *      Author: zawaidhm
+ */
+
+
---
+++ ece230TermProjectMierowIZawaidehH/LevelSensor.h
@@ -0,0 +1,13 @@
+/*
+ * LevelSensor.h
+ *
+ *  Created on: Feb 15, 2024
+ *      Author: zawaidhm
+ */
+
+#ifndef LEVELSENSOR_H_
+#define LEVELSENSOR_H_
+
+
+
+#endif /* LEVELSENSOR_H_ */
